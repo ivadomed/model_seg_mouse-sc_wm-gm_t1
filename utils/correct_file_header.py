4b3584a4f6a20f14b06605579cf1399548ea1297
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2024-01-24 11:59:11 -0500

    fixed correct file header description

--- utils/correct_file_header.py
+++ utils/correct_file_header.py
@@ -4,12 +4,12 @@ This script corrects the file header of nifti files.
 It was specifically written to correct the file header of the nifti files when the resolution of the nifti files is not correct. 
 
 Example of run:
-        $ python utils/correct_file_header.py --path-image /path/to/image --path-out /path/to/output --resolution (0.05, 0.05, 0.05)
+        $ python utils/correct_file_header.py --path-image /path/to/image --path-out /path/to/output --resolution 0.05 0.05 0.05
 
 Arguments:
     --path-image : Path to the individual image to segment.
     --path-out : Path to output directory
-    --resolution : Resolution of the nifti file. Default: (0.05, 0.05, 0.05)
+    --resolution : Resolution of the nifti file. Default: 0.05 0.05 0.05
 
 Returns:
     None
